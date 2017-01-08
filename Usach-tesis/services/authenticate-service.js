618ce53950992dbf16e9fb76ebc83ddcb5e986b0
crisacevedo <cristobal.acevedo@usach.cl>
2017-01-08 17:36:47 -0300

    update de manejo de usuarios
    Ahora login valida si el usuario esta regisrtado o no y no se puede
    entrar sin estar registrado. Falta validar cuando el usuario ya existe
    para no replicar usuarios. ##Hay un error de angular que persiste.

--- Usach-tesis/services/authenticate-service.js
+++ Usach-tesis/services/authenticate-service.js
@@ -2,14 +2,23 @@ app.service('AuthenticationService', ["$http", "$state", function($http, $state)
 	var self = this;
 	self.checkToken = function(token){
 		var data = {token: token};
+		
 		$http.post("endpoints/checkToken.php", data).success(function(response){
-			if (response === "unauthorized"){
+			
+
+			if (response === "authorized"){
+				console.log("Logged in");
+				$state.go("applicaction")
+			} 
+
+
+			if (response !== "authorized"){
 				console.log("Logged out");
 				$state.go("login")
-			} else {
-				console.log("Logged In");
-				return response;
-			}
+			} 
+
+
+
 		}).error(function(error){
 			$state.go("login")
 		})
