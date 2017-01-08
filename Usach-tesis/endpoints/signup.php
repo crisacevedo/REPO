ad6788727e415fabf7e608eeb20049ea2f4a1cb5
crisacevedo <cristobal.acevedo@usach.cl>
2017-01-08 19:16:54 -0300

    Valida que no se repita registro de usuarios

--- Usach-tesis/endpoints/signup.php
+++ Usach-tesis/endpoints/signup.php
@@ -3,6 +3,20 @@
     $data = json_decode(file_get_contents("php://input"));
     $username = $data->username;
     $password = $data->password;
+    
+    $userInfo = $db->query("SELECT * FROM users WHERE email='$username'");
+    $userInfo = $userInfo->fetchAll();
+
+    if (count($userInfo) == 1){
+        //This means that the user is logged in and let's givem a token :D :D :D
+    exit;    
+    }    
+
+
+    if (count($userInfo) !== 1){
+        //This means that the user is logged in and let's givem a token :D :D :D
+    
+    
 
     $q = "INSERT INTO users (email, password) VALUES (:email, :password)";
     $query = $db->prepare($q);
@@ -10,6 +24,6 @@
         ":email" => $username,
         ":password" => sha1($password)
     ));
-
+  }
     echo json_encode($username);
 ?>
