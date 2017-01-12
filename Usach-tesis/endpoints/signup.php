1893d2331afb0d330fc562487800718bc0c7a196
crisacevedo <cristobal.acevedo@usach.cl>
2017-01-12 12:55:13 -0300

    Añadido funcionalidad de mandar email, con phpmailer
    Al registrar un usuario, se manda email con sus credenciales y enlace a
    la pantalla login

--- Usach-tesis/endpoints/signup.php
+++ Usach-tesis/endpoints/signup.php
@@ -1,5 +1,9 @@
 <?php 
     include("../connection.php");
+    require("../class.phpmailer.php");
+    require("../class.smtp.php");
+
+
     $data = json_decode(file_get_contents("php://input"));
     $username = $data->username;
     $password = $data->password;
@@ -24,6 +28,28 @@
         ":email" => $username,
         ":password" => sha1($password)
     ));
-  }
+
+        $mail = new PHPMailer(); // create a new object
+        $mail->IsSMTP(); // enable SMTP
+        $mail->SMTPAuth = true; // authentication enabled
+        $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+        $mail->Host = "smtp.gmail.com";
+        $mail->Port = 465; // or 587
+        $mail->IsHTML(true);
+        $mail->Username = "tesisusachbdd@gmail.com"; //Email that you setup
+        $mail->Password = "T3s4s5s4chBdd"; // Password
+        $mail->Subject = "Registro exitoso en Tesis BDD";
+        $mail->Body ="Hola, bienvenido. Tu usuario es $username y tu clave es $password.Al presionar este link, puedes ingresar a tu sesion http://localhost/Usach-tesis/#/";
+        $mail->AddAddress($username); //Pass the e-mail that you setup
+        
+        if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+
+
+}
+  //}
     echo json_encode($username);
 ?>
