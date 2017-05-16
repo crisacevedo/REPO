c8461e271c3bfb28a6d348bb26fbe8755ee043a1
crisacevedo <cristobal.acevedo@usach.cl>
2017-05-16 16:48:43 -0400

    signup y login arreglado
    ss

--- Usach-tesis/controllers/mainController.js
+++ Usach-tesis/controllers/mainController.js
@@ -1,10 +1,6 @@
 app.controller("MainController", function ($scope, $state, $http, AuthenticationService){
     //If user is not logged in
 
-    $scope.ModificarPassword = {
-        passwordvieja: undefined,
-        passwordnueva: undefined
-       }
 
 	var token;
 	if (localStorage['token']){
@@ -27,24 +23,6 @@ app.controller("MainController", function ($scope, $state, $http, Authentication
 			console.error(error);
 		})
 	}
-
-    $scope.ModificarPassword = function () {
-         var data = {
-            passwordvieja: $scope.ModificarPassword.passwordvieja,
-            passwordnueva: $scope.ModificarPassword.passwordnueva,
-			token: token
-    
-        }
-        
-        $http.post("endpoints/modificarclave.php", data).success(function(response){
-            console.log(response);
-            localStorage.setItem("token", JSON.stringify(response));
-            $state.go("login");
-        }).error(function(error){
-            console.error(error);
-        });
-    
-    }
 
 
 })
--- Usach-tesis/endpoints/signup.php
+++ Usach-tesis/endpoints/signup.php
@@ -1,18 +1,11 @@
 <?php 
     include("../connection.php");
-    require("../class.phpmailer.php");
-    require("../class.smtp.php");
 
 
 
     $data = json_decode(file_get_contents("php://input"));
     $username = $data->username;
     $password = $data->password;
-    $nombre = $data->nombre;
-    $telefono = $data->telefono;
-
-
-
 
 
 
@@ -21,45 +14,21 @@
     $userInfo = $userInfo->fetchAll();
 
     if (count($userInfo) == 1){
-        //This means that the user is logged in and let's givem a token :D :D :D
     exit;    
     }    
 
 
     if (count($userInfo) !== 1){
-        //This means that the user is logged in and let's givem a token :D :D :D
     
     
 
-    $q = "INSERT INTO users (email, password,nombre,telefono) VALUES (:email, :password,:nombre,:telefono)";
+    $q = "INSERT INTO users (email, password) VALUES (:email, :password)";
     $query = $db->prepare($q);
     $execute = $query->execute(array(
         ":email" => $username,
-        ":password" => sha1($password),
-        ":nombre" => $nombre,
-        ":telefono" => $telefono
-    
-    ));
-
-        $mail = new PHPMailer(); // create a new object
-        $mail->IsSMTP(); // enable SMTP
-        $mail->SMTPAuth = true; // authentication enabled
-        $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
-        $mail->Host = "smtp.gmail.com";
-        $mail->Port = 465; // or 587
-        $mail->IsHTML(true);
-        $mail->Username = "tesisusachbdd@gmail.com"; //Email that you setup
-        $mail->Password = "T3s4s5s4chBdd"; // Password
-        $mail->Subject = "Registro exitoso en Tesis BDD";
-        $mail->Body ="Hola, bienvenido. Tu usuario es $username y tu clave es $password.Al presionar este link, puedes ingresar a tu sesion http://localhost/Usach-tesis/#/";
-        $mail->AddAddress($username); //Pass the e-mail that you setup
+        ":password" => sha1($password)
         
-        if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
-
+    ));
 
 }
   //}
